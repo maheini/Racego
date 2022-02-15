7096b5e621a0a30672cfd47a33498e5c4851dc27
maheini35 <maheini35@me.com>
2022-02-15 15:23:05 +0100

    basic cookie handling + api construction

--- lib/data/api/racego_api.dart
+++ lib/data/api/racego_api.dart
@@ -1,10 +1,21 @@
+import 'dart:io';
 import 'package:http/http.dart' as http;
 
 class RacegoApi {
   RacegoApi(this._client);
+  Map<String, String> headers = {};
+  static const String _apiBaseUrl = 'http://localhost/api.php/';
 
-  http.Client _client;
+  final http.Client _client;
 
 
+  void _updateCookie(http.Response response) {
+    String? rawCookie = response.headers['set-cookie'];
+    if (rawCookie != null) {
+      int index = rawCookie.indexOf(';');
+      setCookie((index == -1) ? rawCookie : rawCookie.substring(0, index));
+    }
+  }
 
+  void setCookie(String cookie) => headers['cookie'] = cookie;
 }
