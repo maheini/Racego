c71172499016aafd575ddbe314b52c7c9efd4828
maheini35 <maheini35@me.com>
2022-02-21 21:56:43 +0100

    implemented id and time into cubit directly

--- lib/business_logic/listtoolbar/listtoolbar_cubit.dart
+++ lib/business_logic/listtoolbar/listtoolbar_cubit.dart
@@ -1,5 +1,4 @@
-import 'dart:core';
-
+import 'package:racego/ui/widgets/timeinput.dart';
 import 'package:bloc/bloc.dart';
 import 'package:meta/meta.dart';
 
@@ -9,10 +8,15 @@ class ListToolbarCubit extends Cubit<ListToolbarState> {
   ListToolbarCubit() : super(NoSelection());
   int _currentId = 0;
   bool _isValidLaptime = false;
+  Time _currentTime = Time();
+
+  int getSelectedId() => _currentId;
+  Time getCurrentTime() => _currentTime;
 
   void selectionChanged(int id) {
     _currentId = id;
     _isValidLaptime = false;
+    _currentTime = Time();
     emit(UserSelected(id, userHasChanged: true));
   }
 
@@ -22,9 +26,10 @@ class ListToolbarCubit extends Cubit<ListToolbarState> {
     emit(NoSelection());
   }
 
-  void lapTimeChanged(bool isValid) {
-    if (isValid == _isValidLaptime) return;
-    _isValidLaptime = isValid;
+  void lapTimeChanged(Time time) {
+    _currentTime = time;
+    if (time.isValid == _isValidLaptime) return;
+    _isValidLaptime = !_isValidLaptime;
     emit(
       UserSelected(_currentId,
           validLaptime: _isValidLaptime, userHasChanged: false),
