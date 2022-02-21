f80fc357ed882c286ba89a822551349a0fc6db9b
maheini35 <maheini35@me.com>
2022-02-21 13:47:10 +0100

    finished userlist cubit so far

--- lib/business_logic/userlist_cubit/userlist_cubit.dart
+++ lib/business_logic/userlist_cubit/userlist_cubit.dart
@@ -1,17 +1,70 @@
 import 'package:bloc/bloc.dart';
 import 'package:meta/meta.dart';
+import 'package:racego/data/api/racego_api.dart';
+import 'package:racego/data/exceptions/racego_exception.dart';
+import 'package:racego/data/models/user.dart';
 
 part 'userlist_state.dart';
 
 class UserlistCubit extends Cubit<UserlistState> {
-  UserlistCubit() : super(Loading()) {
+  UserlistCubit(RacegoApi api)
+      : _api = api,
+        super(Loading(const [])) {
     reload();
   }
+
+  final RacegoApi _api;
   String _filter = '';
+  List<User> _newestList = [];
 
-  void reload() async {}
+  /// reloads all user and emit a filtered List
+  ///
+  void reload() async {
+    try {
+      _newestList = await _api.getUser();
+      emit(Loaded(_filter.isNotEmpty
+          ? _filterList(_newestList, _filter)
+          : _newestList));
+    } catch (e) {
+      if (e is RacegoException) {
+        emit(Error(
+            e,
+            _filter.isNotEmpty
+                ? _filterList(_newestList, _filter)
+                : _newestList));
+      } else {
+        UnknownException error = UnknownException(
+            'Unbekannter Fehler', e.toString(), e.runtimeType.toString());
+        emit(Error(
+            error,
+            _filter.isNotEmpty
+                ? _filterList(_newestList, _filter)
+                : _newestList));
+      }
+    }
+  }
 
+  /// set/reset filter. Empty filter means no filter
+  ///
   void setFilter(String filter) {
     _filter = filter;
+    // emit new, filtered list
+
+    if (state is Loaded) {
+      emit(Loaded(_filter.isNotEmpty
+          ? _filterList(_newestList, _filter)
+          : _newestList));
+    }
+  }
+
+  /// function for filtering the list
+  ///
+  List<User> _filterList(List<User> list, String filter) {
+    return _newestList.where(
+      (element) {
+        String fullName = '${element.firstName} ${element.lastName}';
+        return fullName.contains(_filter);
+      },
+    ).toList();
   }
 }
--- lib/business_logic/userlist_cubit/userlist_state.dart
+++ lib/business_logic/userlist_cubit/userlist_state.dart
@@ -3,8 +3,26 @@ part of 'userlist_cubit.dart';
 @immutable
 abstract class UserlistState {}
 
-class Loading extends UserlistState {}
+class Loading extends UserlistState {
+  Loading(List<User> previousList) : _previousList = previousList;
+  final List<User> _previousList;
 
-class Loaded extends UserlistState {}
+  get previousList => _previousList;
+}
 
-class Error extends UserlistState {}
+class Loaded extends UserlistState {
+  Loaded(List<User> list) : _list = list;
+  final List<User> _list;
+  get list => _list;
+}
+
+class Error extends UserlistState {
+  Error(RacegoException exception, List<User> previousList)
+      : _previousList = previousList,
+        _exception = exception;
+  final List<User> _previousList;
+  final RacegoException _exception;
+
+  get exception => _exception;
+  get previousList => _previousList;
+}
