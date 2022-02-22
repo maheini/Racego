488f09cd0ec5daa57d73e62bd7959f5a66d78141
maheini35 <maheini35@me.com>
2022-02-22 14:20:18 +0100

    cancel/finishing lap inside trackcubit

--- lib/business_logic/tracklist_cubit/tracklist_cubit.dart
+++ lib/business_logic/tracklist_cubit/tracklist_cubit.dart
@@ -3,6 +3,7 @@ import 'package:meta/meta.dart';
 import 'package:racego/data/api/racego_api.dart';
 import 'package:racego/data/exceptions/racego_exception.dart';
 import 'package:racego/data/models/user.dart';
+import '../../ui/widgets/timeinput.dart';
 
 part 'tracklist_state.dart';
 
@@ -54,6 +55,64 @@ class TracklistCubit extends Cubit<TracklistState> {
       emit(Loaded(_filter.isNotEmpty
           ? _filterList(_newestList, _filter)
           : _newestList));
+    }
+  }
+
+  /// cancel lap and remove user from track
+  ///
+  void cancelLap(int userId) async {
+    try {
+      bool successful = await _api.cancelLap(userId);
+      if (!successful) {
+        throw DataException(
+            'Benutzer konnte nicht von der Rennstrecke entfernt werden: Id ungültig.');
+      }
+      reload();
+    } catch (e) {
+      if (e is RacegoException) {
+        emit(Error(
+            e,
+            _filter.isNotEmpty
+                ? _filterList(_newestList, _filter)
+                : _newestList));
+      } else {
+        UnknownException error = UnknownException(
+            'Unbekannter Fehler', e.toString(), e.runtimeType.toString());
+        emit(Error(
+            error,
+            _filter.isNotEmpty
+                ? _filterList(_newestList, _filter)
+                : _newestList));
+      }
+    }
+  }
+
+  /// submit lap and remove user from track
+  ///
+  void finishLap(int userId, Time time) async {
+    try {
+      bool successful = await _api.finishLap(userId, time);
+      if (!successful) {
+        throw DataException(
+            'Rundenzeit konnte nicht erfasst werden: Id oder Zeit ungültig.');
+      }
+      reload();
+    } catch (e) {
+      if (e is RacegoException) {
+        emit(Error(
+            e,
+            _filter.isNotEmpty
+                ? _filterList(_newestList, _filter)
+                : _newestList));
+      } else {
+        UnknownException error = UnknownException(
+            'Unbekannter Fehler', e.toString(), e.runtimeType.toString());
+        emit(Error(
+            error,
+            _filter.isNotEmpty
+                ? _filterList(_newestList, _filter)
+                : _newestList));
+      }
     }
   }
 
