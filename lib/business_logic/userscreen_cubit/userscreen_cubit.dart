299d377cf09d2b0974a8900bbc8bd528e756bae7
maheini35 <maheini35@me.com>
2022-02-23 10:24:03 +0100

    userscreenCubit structure given

---
+++ lib/business_logic/userscreen_cubit/userscreen_cubit.dart
@@ -0,0 +1,49 @@
+import 'package:bloc/bloc.dart';
+import 'package:meta/meta.dart';
+import 'package:racego/data/api/racego_api.dart';
+import 'package:racego/data/exceptions/racego_exception.dart';
+import 'package:racego/data/models/userdetails.dart';
+
+part 'userscreen_state.dart';
+
+class UserscreenCubit extends Cubit<UserscreenState> {
+  UserscreenCubit(RacegoApi api)
+      : _api = api,
+        super(UserScreenLoading());
+  final RacegoApi _api;
+
+  /// Load Screen to add a new user
+  ///
+  /// -> use this function if you would like to add a user
+  void loadAddScreen() => emit(UserScreenAdd());
+
+  /// Store a new user
+  ///
+  /// -> use this if you would like to upload the new user
+  void addUser(UserDetails user) async {
+    emit(UserScreenAddSaving());
+
+    // true -> UserScreenEdit
+    // error -> UserScreenAddError
+  }
+
+  /// Load Screen to edit a existing user
+  ///
+  /// -> use this function if you would like to edit a user
+  void loadEditUser(int id) async {
+    emit(UserScreenLoading());
+
+    // true -> UserScreenEdit
+    // error -> UserScreenEditError
+  }
+
+  /// Store a existing user
+  ///
+  /// -> use this if you would like to upload the existing user
+  void editUser() async {
+    emit(UserScreenEditSaving());
+
+    // true -> UserScreenStored
+    // error -> UserScreenEditError
+  }
+}
---
+++ lib/business_logic/userscreen_cubit/userscreen_state.dart
@@ -0,0 +1,20 @@
+part of 'userscreen_cubit.dart';
+
+@immutable
+abstract class UserscreenState {}
+
+class UserScreenAdd extends UserscreenState {}
+
+class UserScreenAddSaving extends UserscreenState {}
+
+class UserScreenAddError extends UserscreenState {}
+
+class UserScreenLoading extends UserscreenState {}
+
+class UserScreenEdit extends UserscreenState {}
+
+class UserScreenEditError extends UserscreenState {}
+
+class UserScreenEditSaving extends UserscreenState {}
+
+class UserScreenStored extends UserscreenState {}
