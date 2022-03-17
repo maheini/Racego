0b8bc832339116180d63edf76fbd7f833d603317
maheini35 <maheini35@me.com>
2022-03-17 19:09:28 +0100

    rankinglist data model

---
+++ lib/data/models/rankinglist.dart
@@ -0,0 +1,22 @@
+import 'package:racego/data/models/user.dart';
+
+class RankingList {
+  RankingList(List<Map<String, String>>? ranks) : _ranks = ranks ?? [];
+
+  final List<Map<String, String>> _ranks;
+
+  Map<String, String> getRank(int rank) {
+    if (rank >= _ranks.length) {
+      return {'dsf': 'sdf'};
+    } else {
+      return _ranks[rank - 1];
+    }
+  }
+
+  RankingList fromJson(List<dynamic> json) {
+    List<Map<String, String>> ranking =
+        json.map((e) => {e['name'].toString(): e['time'].toString()}).toList();
+
+    return RankingList(ranking);
+  }
+}
