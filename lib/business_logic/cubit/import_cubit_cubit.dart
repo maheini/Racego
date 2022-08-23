f997ef1f76fac39678b554f0b9cbfbda11429ae6
maheini35 <maheini35@me.com>
2022-08-23 23:23:31 +0200

    v1 of importcubit

---
+++ lib/business_logic/cubit/import_cubit_cubit.dart
@@ -0,0 +1,86 @@
+import 'dart:io';
+import 'package:bloc/bloc.dart';
+import 'package:csv/csv.dart';
+import 'package:file_picker/file_picker.dart';
+import 'package:meta/meta.dart';
+import 'package:racego/data/api/racego_api.dart';
+import 'package:racego/data/models/userdetails.dart';
+
+part 'import_cubit_state.dart';
+
+class ImportCubit extends Cubit<ImportCubitState> {
+  ImportCubit(this._api) : super(ImportCubitReady());
+  final RacegoApi _api;
+
+  Future<void> importCsv() async {
+    FilePickerResult? result = await FilePicker.platform.pickFiles();
+
+    if (result != null) {
+      File file = File(result.files.single.path ?? '');
+      if (await file.exists()) {
+        String fileContent = await file.readAsString();
+        List<List<dynamic>> content = const CsvToListConverter()
+            .convert(fileContent, fieldDelimiter: ';');
+
+        if (!_checkCsvContent(content)) {
+          emit(ImportCubitReady(message: 'errorrrdfsdfsd'));
+          return;
+        }
+        // upload list
+        List<UserDetails> userDetails = _convertCsvToUserDetails(content);
+
+        // check result
+        int inserted = await _uploadNewUsers(userDetails);
+        emit(ImportCubitReady(
+            message: 'hallofsdfsdfsdf ' + inserted.toString()));
+      } else {
+        // File doesn't exist
+        emit(ImportCubitReady(message: 'hallofsdfsdfsdf'));
+      }
+    } else {
+      // User canceled the picker
+      emit(ImportCubitReady(message: 'hallofsdfsdfsdf'));
+    }
+  }
+
+  bool _checkCsvContent(List<List<dynamic>> content) {
+    for (var element in content) {
+      if (element.length >= 3 &&
+          element.every(
+              (element) => element is String && element.trim().isNotEmpty)) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  List<UserDetails> _convertCsvToUserDetails(List<List<dynamic>> content) {
+    List<UserDetails> userList = [];
+    for (var element in content) {
+      List<String> classes = [];
+      for (var i = 2; i < element.length; i++) {
+        String className = element[i];
+        if (className.trim().isNotEmpty) {
+          classes.add(className);
+        }
+      }
+      UserDetails user = UserDetails(0, element[0], element[1], classes, []);
+      userList.add(user);
+    }
+    return userList;
+  }
+
+  Future<int> _uploadNewUsers(List<UserDetails> userList) async {
+    int success = 0;
+    for (var element in userList) {
+      try {
+        if (await _api.addUser(element) > 0) {
+          success++;
+        }
+      } catch (_) {
+        // Ignored...
+      }
+    }
+    return success;
+  }
+}
