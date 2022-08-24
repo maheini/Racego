395cddd7f8c4abd260502e43669ce1b68b85ad8d
maheini35 <maheini35@me.com>
2022-08-24 23:32:16 +0200

    csvImport specific function for importscreen

--- lib/ui/screens/importscreen.dart
+++ lib/ui/screens/importscreen.dart
@@ -23,25 +23,62 @@ class _ImportScreenState extends State<ImportScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: const RacegoAppBar(title: 'Import-df'),
-      body: Center(
-        child: BlocBuilder<ImportCubit, ImportCubitState>(
-          bloc: _cubit,
-          builder: (context, state) {
-            return Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                ElevatedButton(
-                  onPressed: () => _cubit.importCsv(),
-                  child: const Text('Import starten'),
+
+  Widget _csvImport() {
+    return BlocBuilder<ImportCubit, ImportCubitState>(
+      bloc: _cubit,
+      builder: (context, state) {
+        return Column(
+          children: [
+            TitleBar(
+              S.of(context).csv_import,
+              subtitle: S.of(context).csv_import_description +
+                  '\n\n' +
+                  S.of(context).csv_file_must_contain_headers +
+                  S.current.first_name +
+                  ', ' +
+                  S.of(context).last_name +
+                  ', ' +
+                  S.of(context).race_class +
+                  ', ...,' +
+                  S.of(context).lap +
+                  ', ...',
+            ),
+            const SizedBox(height: 5),
+            if (state is ImportCubitReady && state.message != null)
+              Column(
+                children: [
+                  const SizedBox(height: 10),
+                  Text(
+                    state.message ?? '',
+                  ),
+                  const SizedBox(height: 10),
+                ],
+              ),
+            if (state is ImportCubitLoading)
+              Column(
+                children: const [
+                  SizedBox(height: 10),
+                  CircularProgressIndicator(),
+                  SizedBox(height: 10),
+                ],
+              ),
+            ColoredButton(
+              Text(
+                S.of(context).start_import,
+                style: const TextStyle(
+                  color: Colors.white,
+                  fontSize: 17,
+                  fontWeight: FontWeight.bold,
                 ),
-                if (state is ImportCubitReady && state.message != null)
-                  Text(state.message!),
-              ],
-            );
-          },
-        ),
-      ),
+              ),
+              color: Colors.blue,
+              onPressed: () => _cubit.importCsv(),
+              isDisabled: state is! ImportCubitReady,
+            ),
+          ],
+        );
+      },
     );
   }
 }
